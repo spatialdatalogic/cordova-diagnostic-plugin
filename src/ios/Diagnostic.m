b3427338f86d9ba7be31ebdd408820a953afdc5c
Brian Flood <bFlood@spatialdatalogic.com>
2020-01-17 13:34:17 -0500

    Update Diagnostic.m

--- src/ios/Diagnostic.m
+++ src/ios/Diagnostic.m
@@ -30,11 +30,14 @@ ABAddressBookRef _addressBook;
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     
+    /*
     self.bluetoothManager = [[CBCentralManager alloc]
                              initWithDelegate:self
                              queue:dispatch_get_main_queue()
                              options:@{CBCentralManagerOptionShowPowerAlertKey: @(NO)}];
     [self centralManagerDidUpdateState:self.bluetoothManager]; // Show initial state
+    */
+    
     self.contactStore = [[CNContactStore alloc] init];
     self.motionManager = [[CMMotionActivityManager alloc] init];
     self.motionActivityQueue = [[NSOperationQueue alloc] init];
@@ -251,59 +254,7 @@ ABAddressBookRef _addressBook;
 }
 
 
-#pragma mark - Bluetooth
 
-- (void) isBluetoothAvailable: (CDVInvokedUrlCommand*)command
-{
-    @try {
-        NSString* state = [self getBluetoothState];
-        bool bluetoothEnabled;
-        if([state  isEqual: @"powered_on"]){
-            bluetoothEnabled = true;
-        }else{
-            bluetoothEnabled = false;
-        }
-        [self sendPluginResultBool:bluetoothEnabled :command];
-    }
-    @catch (NSException *exception) {
-        [self handlePluginException:exception :command];
-    }
-}
-
-- (void) getBluetoothState: (CDVInvokedUrlCommand*)command
-{
-    @try {
-        NSString* state = [self getBluetoothState];
-        NSLog(@"%@",[NSString stringWithFormat:@"Bluetooth state is: %@", state]);
-        [self sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:state] :command];
-    }
-    @catch (NSException *exception) {
-        [self handlePluginException:exception :command];
-    }
-    
-}
-
-- (void) requestBluetoothAuthorization: (CDVInvokedUrlCommand*)command
-{
-    @try {
-        NSString* state = [self getBluetoothState];
-        
-        if([state isEqual: @"unauthorized"]){
-            /*
-             When the application requests to start scanning for bluetooth devices that is when the user is presented with a consent dialog.
-             */
-            NSLog(@"Requesting bluetooth authorization");
-            [self.bluetoothManager scanForPeripheralsWithServices:nil options:nil];
-            [self.bluetoothManager stopScan];
-        }else{
-            NSLog(@"Bluetooth authorization is already granted");
-        }
-        [self sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] :command];
-    }
-    @catch (NSException *exception) {
-        [self handlePluginException:exception :command];
-    }
-}
 
 #pragma mark -  Settings
 - (void) switchToSettings: (CDVInvokedUrlCommand*)command
@@ -1024,77 +975,12 @@ ABAddressBookRef _addressBook;
 }
 #endif
 
-- (NSString*)getBluetoothState{
-    NSString* state;
-    NSString* description;
-    
-    switch(self.bluetoothManager.state)
-    {
-            
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        case CBManagerStateResetting:
-#else
-        case CBCentralManagerStateResetting:
-#endif
-            state = @"resetting";
-            description =@"The connection with the system service was momentarily lost, update imminent.";
-            break;
-            
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        case CBManagerStateUnsupported:
-#else
-        case CBCentralManagerStateUnsupported:
-#endif
-            state = @"unsupported";
-            description = @"The platform doesn't support Bluetooth Low Energy.";
-            break;
-            
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        case CBManagerStateUnauthorized:
-#else
-        case CBCentralManagerStateUnauthorized:
-#endif
-            state = @"unauthorized";
-            description = @"The app is not authorized to use Bluetooth Low Energy.";
-            break;
-            
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        case CBManagerStatePoweredOff:
-#else
-        case CBCentralManagerStatePoweredOff:
-#endif
-            state = @"powered_off";
-            description = @"Bluetooth is currently powered off.";
-            break;
-            
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-        case CBManagerStatePoweredOn:
-#else
-        case CBCentralManagerStatePoweredOn:
-#endif
-            state = @"powered_on";
-            description = @"Bluetooth is currently powered on and available to use.";
-            break;
-        default:
-            state = @"unknown";
-            description = @"State unknown, update imminent.";
-            break;
-    }
-    NSLog(@"Bluetooth state changed: %@",description);
-    
-    
-    return state;
-}
+
 
 /********************************/
 #pragma mark - CBCentralManagerDelegate
 /********************************/
 
-- (void) centralManagerDidUpdateState:(CBCentralManager *)central {
-    
-    NSString* state = [self getBluetoothState];
-    NSString* jsString = [NSString stringWithFormat:@"cordova.plugins.diagnostic._onBluetoothStateChange(\"%@\");", state];
-    [self jsCallback:jsString];
-}
+
 
 @end
